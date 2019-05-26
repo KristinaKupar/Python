25d8c380b4a41d68d650f651c02b496583e6fd25
KuparK <k.kupar-2022@kpi.ua>
2019-05-26 23:30:04 +0300

    add comments

--- workshop2/adapter.py
+++ workshop2/adapter.py
@@ -3,9 +3,9 @@ import cx_Oracle
 class ProcedureAdapter:
 
     def call_procedure(self):
-        con = cx_Oracle.connect('pythonhol/welcome@127.0.0.1/orcl')
-        cur = con.cursor()
-        myvar = cur.var(cx_Oracle.NUMBER)
+        con = cx_Oracle.connect('127.0.0.1/orcl')  #localhost
+        cur = con.cursor()     #connect
+        myvar = cur.var(cx_Oracle.NUMBER)    #var for output
         cur.callproc('procedure name', (123, myvar))
         result = myvar.getvalue()
         cur.close()
